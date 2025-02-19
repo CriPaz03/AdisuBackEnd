f0deb55dedb13abb753a56916db2227c3ec0ba54
CriPaz03 <cristianpazienza48@gmail.com>
2025-02-19 17:19:44 +0100

    Url meal

--- adisu/urls.py
+++ adisu/urls.py
@@ -1,21 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from mensa.views import MealViewSet, BookingViewSet, CanteenViewSet, DailyMealViewSet
-from borsa.views import AcademicYearViewSet, IseeRangeViewSet, ScholarshipViewSet, RequestViewSet
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic.base import TemplateView
 
-router = DefaultRouter()
-router.register('academicyear', AcademicYearViewSet)
-router.register('iseerange', IseeRangeViewSet)
-router.register('scholarship', ScholarshipViewSet)
-router.register('request', RequestViewSet)
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('api/', include(router.urls)),
                   path('api/', include('user.urls')),
                   path('api/', include('mensa.urls')),
                   path('gestionale/', include('gestionale.urls')),
--- mensa/urls.py
+++ mensa/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from mensa.views import MealViewSet, DailyMealViewSet, CanteenViewSet, BookingViewSet, RatingViewSet
 
 router = DefaultRouter()
-router.register('meals', MealViewSet)
+router.register('meal', MealViewSet)
 router.register('daily_meals', DailyMealViewSet)
 router.register('canteen', CanteenViewSet)
 router.register('booking', BookingViewSet)
